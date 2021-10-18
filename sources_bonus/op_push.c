396c540ca3b603ad5cdcb6f658f8f2e5462a3c63
wleite <wleite@student.42sp.org.br>
2021-10-18 14:21:34 -0300

    fix: fixed push operation

--- sources_bonus/op_push.c
+++ sources_bonus/op_push.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 10:06:39 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/17 01:42:11 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/18 14:36:20 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ static void	do_push(int **stk_from, int *size_from, int **stk_to, int *size_to)
 {
 	if (*size_from >= 1)
 	{
-		(*stk_to)[*size_to] = (*stk_from)[*size_from - 1];
+		ft_memmove(&(*stk_to)[1], &(*stk_to)[0], sizeof(int) * *size_to);
+		(*stk_to)[0] = (*stk_from)[0];
 		(*size_to)++;
 		(*size_from)--;
+		ft_memmove(&(*stk_from)[0], &(*stk_from)[1], sizeof(int) * *size_from);
 	}
 }
 
