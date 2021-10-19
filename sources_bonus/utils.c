fd89ffaf425d874e8aea9fdc8ac339791a96466f
wleite <wleite@student.42sp.org.br>
2021-10-19 02:23:42 -0300

    fix: fixed the validation of numbers greater or less than an integer

--- sources_bonus/utils.c
+++ sources_bonus/utils.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 10:06:39 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/19 01:39:22 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/19 02:20:06 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 
 int	valid_number(char *nbr)
 {
-	int	i;
-	int	big_digits;
+	int			i;
+	int			big_digits;
+	long int	n;
 
 	big_digits = 0;
 	i = -1;
 	while (nbr[++i] != '\0')
 	{
-		if (!ft_isdigit(nbr[i]) && nbr[i] != '-')
+		if (nbr[i] == '-' && nbr[i + 1] == '\0')
+			return (false);
+		else if (!ft_isdigit(nbr[i]) && nbr[i] != '-')
 			return (false);
 		else if (ft_isdigit(nbr[i]) && nbr[i + 1] == '-')
 			return (false);
 		if (nbr[i] >= '1' && nbr[i] <= '9')
 			big_digits++;
 	}
-	if (ft_atoi(nbr) == 0 && big_digits)
+	n = ft_atol(nbr);
+	if (n > INT_MAX || n < INT_MIN)
+		return (false);
+	if (n == 0 && big_digits)
 		return (false);
 	return (true);
 }
