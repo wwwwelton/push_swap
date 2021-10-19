1ea68bc3d08c6cf6f145757cf8ce36e2e797005a
wleite <wleite@student.42sp.org.br>
2021-10-19 02:38:02 -0300

    fix: changed output to stderror when an error happens

--- sources_bonus/error.c
+++ sources_bonus/error.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 10:06:39 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/16 06:03:24 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/19 02:36:06 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	exit_error(char *message, int status_code, t_data *data)
 {
 	deinit_data(data);
-	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("Error\n", 2);
 	if (message)
 	{
-		ft_putstr_fd(message, 1);
-		ft_putstr_fd("\n", 1);
+		ft_putstr_fd(message, 2);
+		ft_putstr_fd("\n", 2);
 	}
 	exit(status_code);
 }
